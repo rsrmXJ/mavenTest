8dbf585d372a6ef28378758b4247b75727de43a3
rsrmXJ <rsrmxjaosid@gmail.com>
2023-03-08 13:58:29 +0800

    First Test Maven usage2

---
+++ HelloTest/src/main/java/com/atguigu/maven/Hello.java
@@ -0,0 +1,15 @@
+package com.atguigu.maven;
+
+/**
+ * @author : rsrmXJ
+ * @create : 2023/3/7 18:22
+ * @mail : rsrmxjaosid@gmail.com
+ * @version: 1.0
+ */
+public class Hello {
+
+    public String sayHello(String name){
+        return "Hello "+name+"!";
+    }
+
+}
